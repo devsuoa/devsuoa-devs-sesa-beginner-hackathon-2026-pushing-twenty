7547dc074b13e2602303850248bb9f979a67b234
rmen527 <rmen527>
2026-04-18 23:01:28 +1200

    Add basic structure to textboxes

--- src/components/Textbox.tsx
+++ src/components/Textbox.tsx
@@ -9,8 +9,14 @@ interface TextboxProps {
 
 export default function Textbox(props: TextboxProps) {
     return (
-        <div style={{ backgroundColor: "red" }}>
-            {props.text}
+        <div style={{ backgroundColor: props.lightcol }}>
+            <div>
+                <img src={"/" + props.headerimage} alt={props.headerimage} />
+            </div>
+            <div style={{ backgroundColor: props.darkcol }}>
+                <p style={{ fontFamily: "'Fira Code', Arial"}}>{props.text}</p>
+            </div>
+            
         </div>
     )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,26 +10,50 @@ function Home() {
         <div className="flex-1 flex flex-col">
 
           {/* logo */}
-          <div>Logo</div>
+          <div><img src="logo.png" alt="Logo" /></div>
 
           {/* python and alien code */}
           <div className="flex">
             <div className="flex-1">
-              Python Code
+              <Textbox
+                lightcol="#005DA9"
+                darkcol="#002E53"
+                text="Python Code"
+                headerimage="pythoncode.png"
+              />
             </div>
             <div className="flex-1">
-              Alien Code
+              <Textbox
+                lightcol="#00A93D"
+                darkcol="#00531F"
+                text="Alien Code"
+                headerimage="aliencode.png"
+              />
             </div>
           </div>
 
           {/* task */}
-          <div>task</div>
+          <div>
+            <Textbox
+                lightcol="#5E5E5E"
+                darkcol="#2E2E2E"
+                text="Task"
+                headerimage="task.png"
+              />
+          </div>
         </div>
 
         {/* right */}
         <div className="flex-1">
           <div>Code</div>
-          <div>Output</div>
+          <div>
+            <Textbox
+                lightcol="#5E5E5E"
+                darkcol="#2E2E2E"
+                text="Output"
+                headerimage="output.png"
+              />
+          </div>
         </div>
 
 
